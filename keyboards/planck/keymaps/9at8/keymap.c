56647c6821b7363989c4b2ec13c9512f77c3809b
Aditya Thakral <aditya.1998thakral@gmail.com>
2019-07-28 19:52:50 -0500

    :truck: swap nav and num

--- keyboards/planck/keymaps/9at8/keymap.c
+++ keyboards/planck/keymaps/9at8/keymap.c
@@ -65,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Fn 1 | Gui  | Alt  | Ctrl |  NAV |    Space    |  NUM | Alt  | Ctrl | GUI  |      |
+ * | Fn 1 | Gui  | Alt  | Ctrl |  NUM |    Space    |  NAV | Alt  | Ctrl | GUI  |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    FN1,     KC_LGUI, KC_LALT, KC_LCTL, NAV,     KC_SPC,  KC_SPC,  NUM,     KC_RALT, KC_RCTL, KC_RGUI, XXXXXXX
+    FN1,     KC_LGUI, KC_LALT, KC_LCTL, NUM,     KC_SPC,  KC_SPC,  NAV,     KC_RALT, KC_RCTL, KC_RGUI, XXXXXXX
 ),
 
 /* Mac
@@ -83,14 +83,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Fn 1 |      |      |      |MC_NAV|             |MC_NUM|      |      |      |      |
+ * | Fn 1 |      |      |      |MC_NUM|             |MC_NAV|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_MAC] = LAYOUT_planck_grid(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    MC_FN1,  _______, _______, _______, MC_NAV,  _______, _______, MC_NUM,  KC_RGUI, KC_RALT, KC_RCTL, _______
+    MC_FN1,  _______, _______, _______, MC_NUM,  _______, _______, MC_NAV,  KC_RGUI, KC_RALT, KC_RCTL, _______
 ),
 
 /* Nav
