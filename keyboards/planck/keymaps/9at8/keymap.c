167e398822578efe8f57ef5ae1ac3869291165ba
Aditya Thakral <aditya.1998thakral@gmail.com>
2019-11-21 18:31:55 -0500

    v2

--- keyboards/planck/keymaps/9at8/keymap.c
+++ keyboards/planck/keymaps/9at8/keymap.c
@@ -54,6 +54,7 @@ enum planck_keycodes {
 #define NAV MO(_NAV)
 #define NUM MO(_NUM)
 #define FN1 MO(_FN1)
+#define RSHFT_ENT MT(MOD_RSFT, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -61,18 +62,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sh+Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Fn 1 | Gui  | Alt  | Ctrl |  NUM |    Space    |  NAV | Alt  | Ctrl | GUI  |      |
+ * | Fn 1 | Ctrl | Gui  | Alt  |  NUM |    Space    |  NAV | Ctrl | Alt  | Gui  |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    FN1,     KC_LGUI, KC_LALT, KC_LCTL, NUM,     KC_SPC,  KC_SPC,  NAV,     KC_RALT, KC_RCTL, KC_RGUI, XXXXXXX
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSHFT_ENT,
+    FN1,     KC_LCTL, KC_LGUI, KC_LALT, NUM,     KC_SPC,  KC_SPC,  NAV,     KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX
 ),
 
 /* Mac
@@ -131,20 +132,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Num
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   ~  |      |      |      |   (  |   )  | 1  ! | 2  @ | 3  # | =  + | Ins  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      | Del  |      |   {  |   }  | 4  $ | 5  % | 6  ^ | -  _ | \  | |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | Undo | Cut  | Copy |Paste |   [  |   ]  | 7  & | 8  * | 9  ( |   *  | PScr |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Ins  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | 0  ) |      |   /  |      |
+ * | Esc  |      |      |      |      |      |      |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      | PScr |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUM] = LAYOUT_planck_grid(
-    KC_GRV,  KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_1,    KC_2,    KC_3,    KC_EQL,  KC_INS,
-    _______, XXXXXXX, XXXXXXX, KC_DEL,  XXXXXXX, KC_LCBR, KC_RCBR, KC_4,    KC_5,    KC_6,    KC_MINS, KC_BSLS,
-    _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_LBRC, KC_RBRC, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_PSCR,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    XXXXXXX, KC_SLSH, XXXXXXX
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,
+    KC_ESC,  _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Num Mac
